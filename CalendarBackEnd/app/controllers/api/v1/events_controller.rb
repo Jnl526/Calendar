a6b7aea8f864db2c35afeaf868fb6a950f4170ba
Janelle Ballack <janelle@janelleballack.com>
2018-06-21 17:37:43 -0400

    updated api

--- CalendarBackEnd/app/controllers/api/v1/events_controller.rb
+++ CalendarBackEnd/app/controllers/api/v1/events_controller.rb
@@ -1,50 +1,54 @@
 class Api::V1::EventsController < ApplicationController
 
-
+  before_action :set_event, only: [:show, :update, :destroy]
       # GET /events
 
       def index
-        events = Event.order('created_at ASC');
-        render json: {status: 'SUCCESS', message: 'Loaded Events', data:events}, status: :ok
+        @events = Event.order('created_at ASC');
+        render json: @events
       end
 
       # GET /events/1
       def show
-        event = Event.find(params[:id])
-        render json: {status: 'SUCCESS', message: 'Loaded Event', data:event}, status: :ok
+        @event = Event.find(params[:id])
+        render json: @event
       end
 
       # POST /events
 
       def create
-        event = Event.new(event_params)
-        if event.save
-          render json: {status: :created, message: 'Saved Event', data:event}, status: :ok
-        else
-          render json: {status: 'ERROR', message: 'Event not saved', data:event.errors}, status: :unprocessable_entity
-        end
+        @event = Event.new(event_params)
+          if @event.save
+            render json: @event, status: :created, location:        api_v1_event_url(@event)
+          else
+            render json: @event.errors, status: :unprocessable_entity
+          end
       end
       
-      # POST /events
+      # PATCH/PUT /events/1
+
       def update
-        event = Event.find(params[:id])
-        if event.update_attributes(event_params)
-          render json: {status: 'SUCCESS', message: 'Updated Event', data:event}, status: :ok
-        else
-          render json: {status: 'ERROR', message: 'Event not updated', data:event.errors}, status: :unprocessable_entity
-        end
+        @event = Event.find(params[:id])
+          if @event.update(event_params)
+            render json: @event
+          else
+            render json: @event.errors, status: :unprocessable_entity
+          end
       end
       
       # DELETE /events/1
 
       def destroy
         event = Event.find(params[:id])
-        event.destroy
-        render json: {status: :created, message: 'Deleted Event', data:event}, status: :ok
+          @event.destroy
       end
 
       private
       
+      def set_event
+        @event = Event.find(params[:id])
+       end
+
       def event_params
         params.permit(:title, :description, :start_time, :end_time)
       end
